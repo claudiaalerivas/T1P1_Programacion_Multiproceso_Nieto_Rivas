d1621a16cd44cb6fd4d09213d745f282b68785ef
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-10-13 19:31:41 +0200

    fix(ProcessLauncher): modification of document spaces

--- src/org/salesianos/util/ProcessLauncher.java
+++ src/org/salesianos/util/ProcessLauncher.java
@@ -4,11 +4,11 @@ import java.io.File;
 import java.io.IOException;
 
 public class ProcessLauncher {
-  static public Process initIsPrime(int number, String outputFile) throws IOException {
-    final String FILE = "./src/org/salesianos/function/IsPrime.java";
-    ProcessBuilder builder = new ProcessBuilder("java", FILE, String.valueOf(number));
-    builder.redirectOutput(new File(outputFile));
-    builder.redirectError(new File("./output/error/" + outputFile));
-    return builder.start();
-  }
+    static public Process initIsPrime(int number, String outputFile) throws IOException {
+        final String FILE = "./src/org/salesianos/function/IsPrime.java";
+        ProcessBuilder builder = new ProcessBuilder("java", FILE, String.valueOf(number));
+        builder.redirectOutput(new File(outputFile));
+        builder.redirectError(new File("./output/error/" + outputFile));
+        return builder.start();
+    }
 }
